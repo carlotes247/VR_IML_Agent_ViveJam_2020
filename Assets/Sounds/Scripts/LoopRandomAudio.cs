95fddd403ca80e409b3128481fc30b7a4753da6f
Claire <claire.xy@gmail.com>
2020-01-25 22:00:41 +0000

    Basic sounds

---
+++ Assets/Sounds/Scripts/LoopRandomAudio.cs
@@ -0,0 +1,31 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class LoopRandomAudio : MonoBehaviour {
+ 
+     public AudioSource randomSound;
+     public string resourceFolderName;
+     public AudioClip[] audioSources;
+ 
+     // Use this for initialization
+     void Start () {
+				
+		// Create an array
+		Debug.Log(resourceFolderName);
+	     audioSources =  Resources.LoadAll<AudioClip>(resourceFolderName);
+         CallAudio ();
+     }
+ 
+ 
+     private void CallAudio()
+     {
+         Invoke ("RandomSoundness", 3);
+     }
+ 
+     private void RandomSoundness()
+     {
+         randomSound.clip = audioSources[Random.Range(0, audioSources.Length)];
+         randomSound.Play ();
+         CallAudio ();
+     }
+ }
