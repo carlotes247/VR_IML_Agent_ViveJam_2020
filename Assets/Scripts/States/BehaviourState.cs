f251c1ca907afdc323336526d5830d2ad24daa3c
Carlos Gonzalez <cgd506@york.ac.uk>
2020-01-25 18:11:13 +0000

    added a simple state machine logic. currently pressing space triggers the next state and it takes into account some other variables too such as score threshold and whether the transition it's in the middle of the state

---
+++ Assets/Scripts/States/BehaviourState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourState : MonoBehaviour
+{
+    public BehaviourState NextState;
+    public BehaviourState LowScoreNextState;
+    public bool Talking;
+    public bool InMidState;
+
+    public virtual void StateLogic()
+    {
+        //
+    }
+
+    public void StopTalking()
+    {
+        Talking = false;
+        Debug.Log("Agent has stopped talking");
+    }
+
+    public void ContinueTalking()
+    {
+        Talking = true;
+        Debug.Log("Agent has started talking again");
+    }
+
+
+}
