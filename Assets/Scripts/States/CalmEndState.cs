ef9d23c7f60fe0716fdb7ccb8530ed77f3c54d09
Carlos Gonzalez <cgd506@york.ac.uk>
2020-01-26 11:09:46 +0000

    changed a bit the animator controller for the bus and prasm and triggered them from the corrs states

--- Assets/Scripts/States/CalmEndState.cs
+++ Assets/Scripts/States/CalmEndState.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class CalmEndState : BehaviourState
 {
     public string DebugMessage;
+    public Animator BusAnimator;
 
 
     public override void StateLogic()
     {
         Debug.Log(DebugMessage);
         agentAnimator.SetTrigger("06CalmEnd_high");
-
+        agentAnimator.SetTrigger("05NonDisclosure_low");
+        if (BusAnimator != null)
+        {
+            BusAnimator.SetTrigger("StartBus");
+        }
         base.StateLogic();
     }
 }
