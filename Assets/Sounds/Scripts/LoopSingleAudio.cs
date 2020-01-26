4b2da9c71c6365e26fa6f3f8e1526d6011015c29
Claire <claire.xy@gmail.com>
2020-01-26 08:34:37 +0000

    Add good feedback chimes

---
+++ Assets/Sounds/Scripts/LoopSingleAudio.cs
@@ -0,0 +1,28 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class LoopSingleAudio : MonoBehaviour {
+ 
+     public AudioSource soundSource;
+     public int minDur;
+     public int maxDur;
+ 
+     // Use this for initialization
+     void Start () {
+				
+		// Create an array
+         CallAudio ();
+     }
+ 
+ 
+     private void CallAudio()
+     {
+         Invoke ("PlayRandomSound", Random.Range(minDur,maxDur));
+     }
+ 
+     private void PlayRandomSound()
+     {
+         soundSource.Play();
+         CallAudio ();
+     }
+ }
